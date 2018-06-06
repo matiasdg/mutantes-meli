45919acb3fcf817dc88e108aba232acc37bd7adf
Matias Garcia <matiasdanielgarcia@gmail.com>
2018-06-06 00:58:51 -0300

    pruebas

--- config/database.config.js
+++
@@ -1,7 +0,0 @@
-var config = require('../config/app.config.js').get(process.env.NODE_ENV);
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: config.database.url,
-  ssl: true
-});
--- index.js
+++ index.js
@@ -5,6 +5,29 @@ var port = require('./config/app.config.js').get(process.env.NODE_ENV).port;
 
 var app = express();
 
+var config = require('./config/app.config.js').get(process.env.NODE_ENV);
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: config.database.url,
+  ssl: true
+});
+
+
+app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+console.log(result);
+    res.send(result);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.listen(port, function(){
   console.log('Servidor corriendo en el puerto: ' + port + '. Ambiente: ' + process.env.NODE_ENV);
 });
+
