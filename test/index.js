c4e926dad835b02ce1a3f3f6f649989cc7a5d98d
Matias Garcia <matiasdanielgarcia@gmail.com>
2018-06-06 23:46:43 -0300

    Se integra istanbul

--- test/index.js
+++ test/index.js
@@ -1,11 +1,13 @@
 var expect  = require('chai').expect;
 var request = require('request');
+var xMen = require('../models/xMen.model');
 
 describe('Status and content', function() {
+/*
   describe ('Main page', function() {
 	it('status', function(){
 	  request('http://localhost:8010/', function(error, response, body) {
-		expect(response.statusCode).to.equal(200);
+		expect(response.statusCode).to.equal(404);
 	  });
 	});
 	it('content', function() {
@@ -14,11 +16,27 @@ describe('Status and content', function() {
 	  });
 	});
   });
+
   describe ('About page', function() {
 	it('status', function(){
 	  request('http://localhost:8010/about', function(error, response, body) {
 		expect(response.statusCode).to.equal(404);
 	  });
+	});
+  });*/
+
+  describe ('xMen isMutant', function() {
+	it('should return true', function(){
+
+	  var body = {"dna":["ATGCGA","CAGTGC","TTATGT","AGAAGG","CCCCTA","TCACTG"]};
+	  xMen.isMutant(body, function(response){
+
+		expect(response).assert(true);
+
+	  }, function(error){
+
+	  });
+
 	});
   });
 });
